22688d2ef7c005d79af737744549cc98687e6502
Liam1809 <hkhanhlam99@gmail.com>
2022-09-22 03:11:47 +0100

    convert class component to smart component

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,49 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../Components/CardList";
 import ErrorBoundary from "../Components/ErrorBoundary";
 import Scroll from "../Components/Scroll";
 import SearchBox from "../Components/SearchBox";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((values) => this.setState({ robots: values }));
-  }
+      .then((values) => setRobots(values));
+  }, []);
 
-  searchOnChange = (event) => {
-    this.setState({ searchField: event.target.value });
+  const searchOnChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter((robot) =>
-      robot.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+  const filteredRobots = robots.filter((robot) =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
-    return !robots.length ? (
-      <h1 className="f1">Loading...</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">Robots are friendly</h1>
-        <ErrorBoundary>
-          <SearchBox searchOnChange={this.searchOnChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </ErrorBoundary>
-      </div>
-    );
-  }
+  return !robots.length ? (
+    <h1 className="f1">Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robots are friendly</h1>
+      <ErrorBoundary>
+        <SearchBox searchOnChange={searchOnChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
