e492252b233a89189fbe4885a31b0171390a0e0d
Liam1809 <hkhanhlam99@gmail.com>
2022-09-22 19:03:02 +0100

    implementing redux reducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { combineReducers, createReducer } from "redux";
 import ReactDOM from "react-dom/client";
 import App from "./Containers/App";
 import "./index.css";
@@ -6,10 +8,15 @@ import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// apply redux
+const rootReducer = combineReducers({});
+const store = createReducer(rootReducer);
+
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
